1e58ac0ada11e5f79d49321efdf98e4eea4dd708
nalanda <nalandachowdary1993@gmail.com>
2018-10-06 00:05:03 -0400

    completed record class with code to create key, value pairs from the column names and cell values and to return the record values as an array

--- public/index.php
+++ public/index.php
@@ -23,5 +23,22 @@ class recordFactory {
 }
 
 class record {
+    public function __construct(Array $columnNames = null, $cellValues = null ) {
+        $record = array_combine($columnNames, $cellValues);
 
+        foreach ($record as $key => $value){
+            $this -> createProperty($key, $value);
+        }
+    }
+
+    public function createProperty($key = 'key', $value = 'value') {
+        $key = '<th>'. $key . '</th>';
+        $value = '<td>'. $value . '</td>';
+        $this->{$key} = $value;
+    }
+
+    public function returnRecordAsArray(){
+        $array = (array) $this;
+        return $array;
+    }
 }
