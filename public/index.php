d8eb415954a323d482b4f5ee67e15da2218529cf
nalanda <nalandachowdary1993@gmail.com>
2018-10-06 00:25:37 -0400

    added generateHTMLTable function to html class to add the html code and tags to generate the HTML table from the records obtained from CSV file

--- public/index.php
+++ public/index.php
@@ -11,7 +11,33 @@ class main {
 }
 
 class html {
+    public static function generateHTMLTable($records) {
+        $isFirstRecord = true;
+        $table = '<!DOCTYPE html><html lang="en"><head><link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" />
+                    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+                    <script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script></head><body><table class="table table-bordered table-striped">';
 
+        foreach ($records as $record) {
+            $array = $record->returnRecordAsArray();
+            if($isFirstRecord) {
+                $fields = array_keys($array);
+                $table.='<tr>';
+                foreach($fields as $value){
+                    $table .= $value;
+                }
+                $table.= '</tr>';
+                $isFirstRecord = false;
+            }
+            $values = array_values($array);
+            $table.='<tr>';
+            foreach($values as $value){
+                $table .= $value;
+            }
+            $table.= '</tr>';
+        }
+        $table.='</table></body></html>';
+        return $table;
+    }
 }
 
 class csv {
